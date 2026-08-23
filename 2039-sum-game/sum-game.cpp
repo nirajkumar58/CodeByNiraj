55276329ca367c45fd54a600200fd7a020d8f65d
Niraj Kumar <114325923+nirajkumar58@users.noreply.github.com>
2026-08-23 09:38:02 +0530

    Time: 4 ms (47.55%) | Memory: 13.9 MB (92.07%) - LeetSync

---
+++ 2039-sum-game/sum-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool sumGame(string s) {
+       int fcnt=0,scnt=0;
+       int fsum=0,ssum=0;
+       int n=s.size();
+        for(int i=0; i<(n+1)/2; i++){
+            if(s[i]=='?') fcnt++;
+            else{
+                fsum+=s[i]-'0';
+            }
+            if(s[n-i-1]=='?') scnt++;
+            else{
+                ssum+=s[n-i-1]-'0';
+            }
+        }
+        cout<<fcnt<<" "<<scnt<<" "<<ssum<<" "<<fsum<<" "<<(fsum-ssum);
+        int t=(scnt-fcnt)/2;
+        if((fsum-ssum)!=t*9||(fcnt+scnt)%2==1) return true;
+        return false;
+    }
+};
